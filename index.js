9e5715988f9da357b0625b1c51a15aa2205966e4
OBETTA IKENNA <obettaikenna19@gmail.com>
2025-01-18 03:45:58 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+/*
+
+echo "# paystack-Static-account" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nestsoft-dev/paystack-Static-account.git
+git push -u origin main
+
+*/
